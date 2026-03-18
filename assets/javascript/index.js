399ece1ddef1f8c6b2ac3ef509128da0fc611dd9
GiacomoVisin <killerboss1150@gmail.com>
2026-03-18 12:34:49 +0100

    adding first div for the form

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,56 @@
-console.log(`it works`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let form = document.getElementById(`form1`)
+let input = document.getElementById(`firstinput`)
+let card = document.getElementById(`nomeCard`)
+
+
+
+form.addEventListener("submit", function(e){
+    const inputValue = input.value
+    e.preventDefault();
+    card.textContent = inputValue
+    form.reset()
+})
+ */
