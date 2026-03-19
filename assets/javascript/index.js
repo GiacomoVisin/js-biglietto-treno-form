95d07622ca6526f7fe9d7e7d655fdbc65f5e4f87
GiacomoVisin <killerboss1150@gmail.com>
2026-03-19 03:01:15 +0100

    finishing the form

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,8 +1,7 @@
 // initializing the global variables and function
 
 const rate = 0.21
-let finalPrice
-finalPrice = rate * km
+
 
 function generateNumberForCpCode() {
     return Math.floor(10000 + Math.random() * 90000);
@@ -15,28 +14,61 @@ function generateNumberForSeat() {
 
 
 
+
+
+
 // initializing the global variables and function
 
 
 // adding the node of the card and the form
 
 const form = document.getElementById("cardForm")
+const resetButton = document.getElementById("reset")
 let inputPassengerName = document.getElementById("name")
 let CardPassengerName = document.getElementById("passengerName")
 let inputPassengerAge = document.getElementById("age")
+let kmInput = document.getElementById("km")
 let CardSeat = document.getElementById("newSeat")
 let CardCpCode = document.getElementById("newCpCode")
+let ticketPrice = document.getElementById("newPriceTicket")
+let cardOffer = document.getElementById("newOffer")
+let finalPrice
+
 
 // starting the form
 form.addEventListener(`submit`, function (e) {
     e.preventDefault()
+    finalPrice = rate * kmInput.value
+      // condizioni sconto
+    if (inputPassengerAge.value < 18) {
+        finalPrice = finalPrice * 0.8
+        cardOffer.textContent = "Biglietto scontato"
+    } 
+    else if (inputPassengerAge.value > 65) {
+        finalPrice = finalPrice * 0.6
+           cardOffer.textContent = "Biglietto scontato"
+    }
+     else {
+        cardOffer.textContent = "Biglietto standard"
+    }
+
     let cpCode = generateNumberForCpCode()
     let seat = generateNumberForSeat()
     let inputPassengerNameValue = inputPassengerName.value
     CardPassengerName.textContent = inputPassengerNameValue
     CardSeat.textContent = seat
     CardCpCode.textContent = cpCode
+    ticketPrice.textContent = finalPrice.toFixed(2) + `€`
     form.reset()
+})
+
+
+resetButton.addEventListener('click', function() {
+    CardPassengerName.textContent = ""
+    CardSeat.textContent = ""
+    CardCpCode.textContent = ""
+    ticketPrice.textContent = ""
+    cardOffer.textContent = ""
 })
 
 
@@ -71,22 +103,3 @@ form.addEventListener(`submit`, function (e) {
 
 
 
-
-
-
-
-
-
-/* let form = document.getElementById(`form1`)
-let input = document.getElementById(`firstinput`)
-let card = document.getElementById(`nomeCard`)
-
-
-
-form.addEventListener("submit", function(e){
-    const inputValue = input.value
-    e.preventDefault();
-    card.textContent = inputValue
-    form.reset()
-})
- */
